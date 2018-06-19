5af9cd33690426609a5ee4bc603a1f8f3dc2f997
Guilherme Pim <guilhermeopim@gmail.com>
2018-06-18 21:44:39 -0300

    criar login

---
+++ client/accounts.js
@@ -0,0 +1,40 @@
+T9n.setLanguage('pt');
+
+AccountsTemplates.configure({
+    defaultLayoutType: 'blaze', 
+    defaultLayout: 'LayoutPrincipal',
+    defaultLayoutRegions: {
+        nav: 'Cabecalho',
+    },
+    defaultContentRegion: 'main'
+});
+
+AccountsTemplates.configureRoute('signIn');
+
+var pwd = AccountsTemplates.removeField('password');
+AccountsTemplates.removeField('email');
+AccountsTemplates.addFields([
+  {
+      _id: "username",
+      type: "text",
+      displayName: "username",
+      required: true,
+      minLength: 3,
+  },
+  {
+      _id: "email",
+      type: "email",
+      required: true,
+      displayName: "email",
+      re: /.+@(.+){2,}\.(.+){2,}/,
+      errStr: "E-mail inválido",
+  },
+  pwd,
+  {
+      _id: "Nome",
+      type: "text",
+      displayName: "Nome Completo",
+      required: true,
+      minLength: 3,
+  },
+]);
