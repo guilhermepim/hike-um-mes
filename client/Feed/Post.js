d0a1f3569673196de12327cc2d2e161cceb6275c
Guilherme Pim <guilhermeopim@gmail.com>
2018-06-23 16:00:16 -0300

    Implementar curtidas

---
+++ client/Feed/Post.js
@@ -0,0 +1,30 @@
+Template.Post.helpers({
+	usernameDoAutor: function() {
+		let idDoAutor = this.idDoAutor;
+		let autor = Meteor.users.findOne({_id: idDoAutor});
+		return autor.username;
+	},
+	numeroDeCurtidas: function() {
+		return this.curtidas.length;
+	},
+	usuarioCurtiu: function() {
+		let curtidas = this.curtidas;
+		let posicao = curtidas.indexOf(Meteor.userId());
+
+		if(posicao === -1) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+});
+
+Template.Post.events({
+	"click .botao-curtir": function(evento, template) {
+		Meteor.call("curtirPost", template.data._id);
+	},
+	"click .botao-descurtir": function(evento, template) {
+		Meteor.call("descurtirPost", template.data._id);
+	}
+});
