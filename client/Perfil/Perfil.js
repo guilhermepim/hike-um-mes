8c2b759c6f2c2b13b0c92a47498560eaaf6c88ea
Guilherme Pim <guilhermeopim@gmail.com>
2018-06-24 20:51:30 -0300

    Implementar funcionalidade de seguir e deixar de seguir usuários com ajustes

--- client/Perfil/Perfil.js
+++ client/Perfil/Perfil.js
@@ -8,6 +8,38 @@ Template.Perfil.helpers({
 		let idDoUsuario = FlowRouter.getParam("id");
 		let postsDoPerfil = Posts.find({idDoAutor: idDoUsuario}).fetch();
 		return postsDoPerfil;
+	},
+	segue: function() {
+		let idDoUsuario = FlowRouter.getParam("id");
+		let usuario = Meteor.users.findOne({_id: idDoUsuario});
+		let seguidores = usuario.profile.seguidores;
+		let posicao = seguidores.indexOf(Meteor.userId());
+
+		if(posicao === -1) {
+			return false;
+		} else {
+			return true;
+		}
+	},
+	euMesmo: function() {
+		let idDoUsuario = FlowRouter.getParam("id");
+		if(idDoUsuario === Meteor.userId()) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 });
 
+Template.Perfil.events({
+	"click .seguir": function(evento, Template) {
+		let idDoUsuario = FlowRouter.getParam("id");
+		Meteor.call("seguirUsuario", idDoUsuario);
+		console.log("seguindo");
+	},
+	"click .deixar-de-seguir": function(evento, Template) {
+		let idDoUsuario = FlowRouter.getParam("id");
+		Meteor.call("deixarDeSeguirUsuario", idDoUsuario);
+		console.log("deixando de seguir");
+	}
+});
