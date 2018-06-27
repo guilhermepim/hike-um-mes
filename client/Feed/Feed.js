dcbe4ed3a70ccd642dc500fd52bc0c4c98e39594
Guilherme Pim <guilhermeopim@gmail.com>
2018-06-26 23:59:38 -0300

    Remover autopublish, implementar publications e subscriptions

--- client/Feed/Feed.js
+++ client/Feed/Feed.js
@@ -1,3 +1,11 @@
+Template.Feed.onCreated(function() {
+	Meteor.subscribe("posts");
+});
+
+
+Meteor.subscribe("usuarios");
+
+
 Template.Feed.helpers({
 	posts: function() {
 		let postsDaCollection = Posts.find().fetch().reverse();
